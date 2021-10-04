3b254606f8360c27499a5833f3ce8a1339566364
Dain Kim <dev.dain.k@gmail.com>
2021-10-04 11:17:08 +0900

    2021-10-04 백준

---
+++ boj/2133.py
@@ -0,0 +1,13 @@
+# 09-06, 10-04
+n = int(input())
+if n % 2:
+  print(0)
+else:
+  dp = [0] * (n+1)
+  dp[2] = 3
+  for i in range(4, n+1, 2):
+    dp[i] = dp[i-2] * 3
+    for j in range(4, i, 2):
+      dp[i] += dp[i-j] * 2
+    dp[i] += 2
+  print(dp[n])
